90e16f6c095c1d21e99c50977cbac9684df00afb
adnankaif1 <adnankaif999@gmail.com>
2024-09-03 23:46:57 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,4 @@
+#Simple Method-----------------
 operator = input("Enter an operator ( + - * /): ")
 num1 = float(input ("Enter Number 1:"))
 num2 = float(input ("Enter NUmber 2:"))
@@ -13,3 +14,40 @@ elif operator == "/":
     print( round(result, 3))
 else:
     print (f" {operator} is not a valid operator")
+
+
+#Another code ------------------------- Another code with defination method
+
+
+#Calculator
+def add (x, y):
+    return x+y
+
+def minus ( x, y):
+    return x-y 
+
+print("Select option")
+print ("1.add")
+print ("2.minus")
+
+while True:
+    Select = input("slecct number (1.Addition/ 2.Minus):") 
+    if Select in ('1','2'):
+        try: 
+            num1 = int(input ("Enter Number 1:")) 
+            num2 = int(input("Enter Number 2:") ) 
+        except:
+            print ("Invalid number")
+            continue
+        if Select == '1':
+            print (add(num1, num2))
+
+        elif Select == '2':
+            print (minus (num1, num2))
+
+        next_calculation = input ("yes/no:")   
+        if next_calculation == "no":
+            break
+
+    else:
+        print("Invalid numbers")            
